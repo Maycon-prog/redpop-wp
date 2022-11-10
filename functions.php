65ace2e819c59ba082bbdaabfc5c560e52abea9d
Maycon Duarte <mgam2004@gmail.com>
2022-11-10 15:00:06 -0300

    up

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function theme_add_scripts()
     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('bootstrap-icons','https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'); 
     wp_enqueue_style('style-css', get_template_directory_uri() . '/style.css');
-    wp_enqueue_script('scripts-js', get_template_directory_uri() . '/js/scripts.js');
+    //wp_enqueue_script('scripts-js', get_template_directory_uri() . '/js/scripts.js');
     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js');
     wp_enqueue_script('vuejs', 'https://unpkg.com/vue@3/dist/vue.global.js');
     wp_enqueue_script('splider-js', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js');
@@ -33,19 +33,3 @@ function theme_add_config()
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'theme_add_config', 0);
-
-
-function add_elementor_widget_categories($elements_manager)
-{
-
-    $elements_manager->add_category(
-        'widgets-personalizados',
-        [
-            'title' => esc_html__('Widgets Personalizados', 'elementor'),
-            'icon' => 'eicon-global-colors',
-        ]
-    );
-}
-add_action('elementor/elements/categories_registered', 'add_elementor_widget_categories');
-
-require_once("custom-widgets/my-widgets.php");
