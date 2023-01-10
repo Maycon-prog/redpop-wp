78055557438af257eecee3ab584cd72973a9e138
Adriel Cavalcante <adriel.cavalcante2004@gmail.com>
2023-01-10 16:01:57 -0300

    Atualizando as img's do footer

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
             Realização
         </strong>
         <div class="realizacao">
-            <a href="https://portal.fiocruz.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2022/11/NOVO-Fiocruz-Bahia-LOGO-IGM-1.jpg" alt="Fundação Oswaldo Cruz"></a>
-            <a href="https://www.coc.fiocruz.br/index.php/pt" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2022/11/casa-1.jpg" alt="Casa de Oswaldo Cruz"></a>
-            <a href="https://www.museudavida.fiocruz.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2022/11/Sem-Titulo-4-1.jpg" alt="Museu da Vida"></a>
-            <a href="https://redpop.lat" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2022/11/REDPOPtxt-1.jpg" alt="RedPop"></a>
+            <a href="https://portal.fiocruz.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2023/01/Captura-de-Tela-2023-01-10-às-15.53.55.png" alt="Fundação Oswaldo Cruz"></a>
+            <a href="https://www.coc.fiocruz.br/index.php/pt" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2023/01/Captura-de-Tela-2023-01-10-às-15.54.16.png" alt="Casa de Oswaldo Cruz"></a>
+            <a href="https://www.museudavida.fiocruz.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2023/01/Captura-de-Tela-2023-01-10-às-15.54.29.png" alt="Museu da Vida"></a>
+            <a href="https://redpop.lat" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2023/01/Captura-de-Tela-2023-01-10-às-15.54.38.png" alt="RedPop"></a>
         </div>
     </div>
 
@@ -16,8 +16,8 @@
             Apoio
         </strong>
         <div class="apoio">
-            <a href="http://www.cnpq.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2022/11/cnpq-logo-1-1.jpg" alt="Conselho Nacional de Desenvolvimento Científico e Tecnológico"></a>
-            <a href="https://www.faperj.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2022/11/logo_faperj_preto_completo-1.jpg" alt="Fundação carlos chagas Filho de Amparo á pesquisa do Estado do Rio de Janeiro"></a>
+            <a href="http://www.cnpq.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2023/01/Captura-de-Tela-2023-01-10-às-15.54.52.png" alt="Conselho Nacional de Desenvolvimento Científico e Tecnológico"></a>
+            <a href="https://www.faperj.br" target="_blank"><img class="img-footer" src="http://redpop.fiocruz.br/wp-content/uploads/2023/01/Captura-de-Tela-2023-01-10-às-15.55.06.png" alt="Fundação carlos chagas Filho de Amparo á pesquisa do Estado do Rio de Janeiro"></a>
         </div>
     </div>
 </footer>
